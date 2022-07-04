42e2e28dd1a346b8539bd088ba4d70ad2b9b21b4
Karthick-Maker <74721611+Karthick-Maker@users.noreply.github.com>
2022-07-04 12:36:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,28 +3,24 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-#TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-TG_BOT_TOKEN = "5588177593:AAGwl2Ot_ChKNzyHRzEXRSM7VgUCnlKevCc"
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-#APP_ID = int(os.environ.get("APP_ID", ""))
-APP_ID = int("12610835")
+APP_ID = int(os.environ.get("APP_ID", ""))
 
 #Your API Hash from my.telegram.org
-#API_HASH = os.environ.get("API_HASH", "")
-API_HASH = "886233f38d7d06f369974fddb7842f36"
+API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
-#CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-CHANNEL_ID = int("-1001485941401")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+
 
 #OWNER ID
-#OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-OWNER_ID = int("5398449311")
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Database 
-#DB_URI = os.environ.get("DATABASE_URL", "")
-#DB_URI = ""
+DB_URI = os.environ.get("DATABASE_URL", "")
+
 
 #force sub channel id, if you want enable force sub
 #FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
@@ -32,6 +28,7 @@ FORCE_SUB_CHANNEL = int("0")
 
 #TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 TG_BOT_WORKERS = int("4")
+
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
